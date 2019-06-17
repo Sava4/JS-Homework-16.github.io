3c483a48464e0ba7e02e158522f00076bd62b8db
Alexander Savchenko <sasha.savchenko@gmail.com>
2019-06-17 16:21:24 +0300

    Factorial complete

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert('Hello world!');
+// Напишите как вы понимаете рекурсию. Для чего она используется на практике?
+// Рекурсивная функция это функция которая вызывает сама себя. 
+// На практике используется когда некий алгоритм поиска решения проще выразить разбивая его на меньшие и меньшие части с помощью одного и того же метода
+// Часто используется в функциональном программировании, обходе графов, глубоком копировании объектов
+// В браузерных движках JS есть ограничения (~10 000) по количеству рекурсивных вызовов которые сохраняются в стеке.
+
+let number = parseInt(prompt('please enter number', '8'));
+
+let notValidNum = num => isNaN (num);
+
+while (notValidNum(number)) {
+    number = parseInt(prompt('Please enter integer number once again', '8'));
+}
+
+let factorial = (num) => {
+    if (num === 1) {
+        return num;
+    } else { return (num = num * factorial(num - 1))}
+}
+
+document.getElementById('main').innerHTML = `The factorial of ${number} is: ${factorial(number)}`;
